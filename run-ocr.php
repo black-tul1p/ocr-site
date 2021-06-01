dd2b355abba1e7fd4460dc0150370eed7550eb82
black-tul1p <67044218+black-tul1p@users.noreply.github.com>
2021-06-01 12:53:49 +0400

    Fixed code logic

--- run-ocr.php
+++ run-ocr.php
@@ -1,60 +1,66 @@
 <?php
-	include 'upload.php';
-	// if (strcmp($fileDest, "") !== 0) {
-	if (true) {
-		// echo shell_exec("python ./OCR/digit_detect.py ".$fileDest);
-		echo '<!DOCTYPE html>
-<link rel="stylesheet" type="text/css" href="./Styles/styles.css" />
-<html lang="en-US">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Digit Recognizer</title>
-	</head>
-	<body>
-		<!-- Title -->
-		<header>
-			<div class="main-title teal-bg"><br><center>
-				<!-- <img src="./Images/logo.png" alt="Digit OCR logo"> -->
-				<a href="./">Digit Recognizer</a>
-			</center><br></div>
-		</header>
+// Main site source code
+echo '<!DOCTYPE html>
+		<link rel="stylesheet" type="text/css" href="./Styles/styles.css" />
+		<html lang="en-US">
+			<head>
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Digit Recognizer</title>
+			</head>
+			<body>
+				<!-- Title -->
+				<header>
+					<div class="main-title teal-bg"><br><center>
+						<!-- <img src="./Images/logo.png" alt="Digit OCR logo"> -->
+						<a href="./">Digit Recognizer</a>
+					</center><br></div>
+				</header>
 
-		<br><br><br><br><br>
+				<br><br><br><br><br>
 
-		<!-- Flexbox -->
+				<!-- Flexbox -->
 
-			<!-- Form for URL submission and refresh button -->
-			<div class="info-card" style="flex-basis: 40em; flex-grow: 1; flex-shrink: 1;" id="image-holder"><br>
-				<!-- URL upload form -->
-				<form id="upload">
-					<input type="text" value="" id="image-url" placeholder="Enter URL to image..." style="margin: 5px; width: 8.5vw;">
-					<input type="button" id="btn" class="click-button" value="SELECT" onclick="loadLink();">
+					<!-- Form for URL submission and refresh button -->
+					<div class="info-card" style="flex-basis: 40em; flex-grow: 1; flex-shrink: 1;" id="image-holder"><br>
+						<!-- URL upload form -->
+						<form id="upload">
+							<input type="text" value="" id="image-url" placeholder="Enter URL to image..." style="margin: 5px; width: 8.5vw;">
+							<input type="button" id="btn" class="click-button" value="SELECT" onclick="loadLink();">
 
-					<!-- <font style="opacity: 35%; font-size: 0.55em; font-family: sans-serif; font-weight: bold;"> OR&nbsp; </font> -->
-					<br><p class="bg-text">OR</p>
+							<!-- <font style="opacity: 35%; font-size: 0.55em; font-family: sans-serif; font-weight: bold;"> OR&nbsp; </font> -->
+							<br><p class="bg-text">OR</p>
 
-				<!-- 
-					<input type="file" id="uploaded-img" hidden="hidden">
-					<button type="button" id="click-button" class="click-button">CHOOSE A FILE</button>
-					<span id="upload-text">No file selected.</span><br>
-				-->
-				</form>
-				<!-- Image upload button -->
-				<form action="upload.php" method="POST" enctype="multipart/form-data">
-					<input type="file" name="file" style="width: 180px"><button type="submit" name="submit">UPLOAD</button>
-				</form>
+						<!-- 
+							<input type="file" id="uploaded-img" hidden="hidden">
+							<button type="button" id="click-button" class="click-button">CHOOSE A FILE</button>
+							<span id="upload-text">No file selected.</span><br>
+						-->
+						</form>
+						<!-- Image upload button -->
+						<form action="upload.php" method="POST" enctype="multipart/form-data">
+							<input type="file" name="file" style="width: 180px"><button type="submit" name="submit">UPLOAD</button>
+						</form>
 
-				<!-- Analyze image button -->
-				<form action="run-ocr.php" method="POST">
-					<br>
-					<input type="submit" id="run-button" value="IDENTIFY!" onclick="loadImage();">
-					<br><br>
-				</form>';
+						<!-- Analyze image button -->
+						<form action="run-ocr.php" method="POST">
+							<br>
+							<input type="submit" id="run-button" value="IDENTIFY!" onclick="loadImage();">
+						</form>
+';
 
-		echo shell_exec("python ./OCR/digit_detect.py ./Uploads/image_3.png 2>&1");
+if (isset($_POST['image-path'])) {
+	// Variable to hold path to image
+	$fileDest = $_POST['image-path'];
 
-		echo '<br><br>	</div>
+	// Running the ML script on the image
+	echo "<p>".shell_exec("python ./OCR/digit_detect.py ".$fileDest)."</p>";
+
+} else {
+	echo "<p class='bg-text'>No image was uploaded</p>";
+}
+// Main site source code
+echo '<br></div>
 		<!-- Script for loading image -->
 			<div>
 				<script type="text/javascript">
@@ -88,7 +94,4 @@
 			</div>
 	</body>
 </html>';
-	} else {
-		header("Location: index.php?noimage");
-	}
 ?>
